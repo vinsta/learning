f1702371a6465599efc01857d4cdbafed5511133
Barry Song <21cnbao@gmail.com>
2015-10-19 17:04:34 +0800

    code when reading the book 'Operating Systems - Three Easy Pieces'

--- book/OperatingSystemsThreeEasyPieces/cpu.c
+++ book/OperatingSystemsThreeEasyPieces/cpu.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <assert.h>
-#include "common.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +11,7 @@ int main(int argc, char *argv[])
     char *str = argv[1];
     while (1)
     {
-        Spin(1);
+        sleep(1);
         printf("%s\n", str);
     }
     return 0;
